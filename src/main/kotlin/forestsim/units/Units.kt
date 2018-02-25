112bc5f2527e68270467daf27c5d198f1c95dad9
Emre <emrsmsrli1@gmail.com>
2018-02-26 00:36:35 +0300

    forestsim - oop design to units

--- src/main/kotlin/forestsim/units/Units.kt
+++ src/main/kotlin/forestsim/units/Units.kt
@@ -1,34 +1,45 @@
 package forestsim.units
 
-import clamp
 import forestsim.Forest
 import forestsim.Terminal
 import org.codetome.zircon.api.color.TextColor
 import org.codetome.zircon.api.color.TextColorFactory
 
-data class Coordinate(private val y: Int, private val x: Int) {
+class Coordinate(private val y: Int, private val x: Int) {
     fun around(): Coordinate {
         val xoff = Forest.rand.nextInt(3) - 1
         val yoff = Forest.rand.nextInt(3) - 1
         return Coordinate(
-                clamp(0, y + yoff, Forest.SIZE),
-                clamp(0, x + xoff, Forest.SIZE))
+                (y + yoff + Forest.SIZE) % Forest.SIZE,
+                (x + xoff + Forest.SIZE) % Forest.SIZE)
     }
 
     override fun equals(other: Any?): Boolean {
         if(other == null || other !is Coordinate) return false
         return other.x == x && other.y == y
     }
+
+    override fun hashCode(): Int {
+        var result = y
+        result = 31 * result + x
+        return result
+    }
+
+    companion object {
+        fun random(): Coordinate {
+            return Coordinate(Forest.rand.nextInt(Forest.SIZE), Forest.rand.nextInt(Forest.SIZE))
+        }
+    }
 }
 
 interface IUnit {
-    var coordinate: Coordinate
+    var location: Coordinate
     val color: TextColor
     fun draw() {
         Terminal.draw(color)
     }
     fun move(c: Coordinate) {
-        coordinate = c
+        location = c
     }
 }
 
@@ -36,39 +47,72 @@ interface ITree : IUnit {
     var age: Int
 }
 
-interface IGrowableTree : ITree {
+interface IGrowableTree<out T : IHarvestableTree> : ITree {
     fun isGrown(): Boolean
+    fun grow(): T
 }
 
-interface ISpawnerTree
+interface ISpawnerTree : ITree {
+    fun canSpawnSapling(): Boolean
+}
 
-class Sapling(override var coordinate: Coordinate) : IGrowableTree {
+interface IHarvestableTree : ISpawnerTree {
+    fun harvest(): Int
+}
+
+class Sapling(override var location: Coordinate) : IGrowableTree<Tree> {
     override var age = 0
     override val color = TextColorFactory.fromRGB(50, 205, 50)
 
     override fun isGrown(): Boolean {
         return age == 12
     }
+
+    override fun grow(): Tree {
+        return Tree(location)
+    }
 }
 
-class Tree(override var coordinate: Coordinate) : IGrowableTree, ISpawnerTree {
+class Tree(override var location: Coordinate) : IGrowableTree<ElderTree>, IHarvestableTree {
     override var age = 0
     override val color = TextColorFactory.fromRGB(34, 139, 34)
+
+    override fun canSpawnSapling(): Boolean {
+        return Forest.rand.nextFloat() <= .1
+    }
 
     override fun isGrown(): Boolean {
         return age == 120
     }
+
+    override fun grow(): ElderTree {
+        return ElderTree(location)
+    }
+
+    override fun harvest(): Int {
+        return 1
+    }
 }
 
-class ElderTree(override var coordinate: Coordinate) : ITree, ISpawnerTree {
+class ElderTree(override var location: Coordinate) : IHarvestableTree {
     override var age = 0
     override val color = TextColorFactory.fromRGB(0, 100, 0)
+
+    override fun canSpawnSapling(): Boolean {
+        return Forest.rand.nextFloat() <= .2
+    }
+
+    override fun harvest(): Int {
+        return 2
+    }
 }
 
-class Lumberjack(override var coordinate: Coordinate) : IUnit {
+class Lumberjack(override var location: Coordinate) : IUnit {
+    var lumberHarvested = 0
     override val color = TextColorFactory.fromRGB(255, 255, 255)
 }
 
-class Bear(override var coordinate: Coordinate) : IUnit {
-    override val color = TextColorFactory.fromRGB(178, 34, 34)
+class Bear(override var location: Coordinate) : IUnit {
+    var lumberjacksMawed = 0
+    override val color = TextColorFactory.fromRGB(139, 0, 0)
 }
